86b3b6003c0f02bbbbd00890093575ff0b54c7db
NXVZBGBFBEN <nxvzbgbfben@gmail.com>
2024-08-27 19:41:13 +0900

    add useState()

--- components/gpsButton.tsx
+++ components/gpsButton.tsx
@@ -1,13 +1,19 @@
 import { useMap } from "react-leaflet";
 import Control from "react-leaflet-custom-control";
 import { MdMyLocation } from "react-icons/md";
+import { useState } from "react";
 
 export default function GpsButton() {
+  const [_latitude, setLatitude] = useState<number>();
+  const [_longitude, setLongitude] = useState<number>();
+
   const mapComponent = useMap();
   const setLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(({ coords }) => {
-        mapComponent.flyTo([coords.latitude, coords.longitude], mapComponent.getZoom());
+        setLatitude(coords.latitude);
+        setLongitude(coords.longitude);
+        mapComponent.flyTo([coords.latitude, coords.longitude], 11);
       });
     }
   };
